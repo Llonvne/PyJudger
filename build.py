b4a6bb5c74c59840dacc5c8aad9e883d5ee1b47d
Llonvne <work@llonvne.cn>
2023-04-02 16:53:43 +0800

    Update build.py

--- build.py
+++ build.py
@@ -1,7 +1,4 @@
-import subprocess
+import os
 
 if __name__ == "__main__":
-    program = subprocess.Popen("pyinstaller --onefile --name llonvne_judger_server api.py"
-                               , shell=True, text=True, stderr=subprocess.PIPE,stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-    program.wait()
-    print(program.stdout.read())
+    os.system("pyinstaller --onefile --name llonvne_judger_server api.py")
